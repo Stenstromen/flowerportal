5d88d9b1ea1b88357c57612d802bd6e210ce83fc
Stenstromen <peppoji@gmail.com>
2022-05-19 00:01:30 +0200

    Abandoned flower API idea and continued with 'imagedb' array with math.random

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtitle=Category:Flowers_of_Sweden&gcmlimit=500&gcmtype=file&prop=imageinfo&&iiprop=url&format=json
 
+/*
 async function getFlower() {
     let response = await fetch("https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtitle=Category:Flowers_of_Sweden&gcmlimit=10&gcmtype=file&prop=imageinfo&&iiprop=url&format=json");
     let data = await response.json();
@@ -7,8 +8,27 @@ async function getFlower() {
 }
 
 getFlower().then(data => console.log(data));
+*/
+
+const imagedb = [
+    {
+        img: "https://http.cat/200.jpg",
+        desc: "OK Cat"
+    },
+    {
+        img: "https://http.cat/503.jpg",
+        desc: "503 Cat"
+    }
+]
+
+const randomize = imagedb.length * Math.random();
+const randomimg = Math.floor(randomize);
 
 const image = document.createElement("img");
-image.setAttribute("src", "https://http.cat/200.jpg");
-image.setAttribute("alt", "cat");
+image.setAttribute("src", imagedb[randomimg].img);
+image.setAttribute("alt", imagedb[randomimg].desc);
 document.getElementById("flower").appendChild(image);
+
+console.log(randomimg)
+
+console.log(imagedb[randomimg].img)
